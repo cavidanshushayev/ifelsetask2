01145abd9e2b450c7b335982fbd6a2d9f58f54ac
Cavidan Shushayev <csusayev19@gmail.com>
2023-02-25 10:44:43 +0400

    if else task2 commit

---
+++ homework5/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace homework4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zehmet olmasa parklanma saatin qeyd edin");
+
+            double saat = Convert.ToDouble(Console.ReadLine());
+            if (saat < 3)
+            {
+                Console.WriteLine("odenilecek mebleg  " + saat + "azn");
+            }else if(saat >3 && saat <= 5)
+            {
+                double endrimsizmebleg = 3;
+                double endrimlimebleg = (saat - endrimsizmebleg) * 80 / 100;
+                double tammebleg = endrimsizmebleg + endrimlimebleg;
+                Console.WriteLine("odenilecek mebleg   " + tammebleg + "azn");
+            }else if(saat > 5 && saat <= 8)
+            {
+                double endrimsizmebleg = 4.6f;
+
+                double secondendrimlimebleg = (saat - 5) * 50 / 100;
+                double  tammebleg = endrimsizmebleg +secondendrimlimebleg;
+                Console.WriteLine("odenilecek mebleg " + tammebleg + "azn");
+            }else
+            {
+                double endrimsizmebleg = 6.1f;
+                double endrimlimebleg = (saat - 8) * 75 / 100;
+                double tammebleg = endrimlimebleg + endrimsizmebleg;
+                Console.WriteLine("odenilecek mebleg " + tammebleg + "azn");
+
+            }
+        }
+    }
+}
